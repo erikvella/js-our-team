dbff1f5a69d79de74831f27ef61d4acf1ace8d40
erikvella <vella.erik1@gmail.com>
2023-09-21 14:49:46 +0100

    [add] DOM printed

--- README.md
+++ README.md
@@ -18,9 +18,17 @@ Prima la logica in italiano e poi traduciamo in codice.
 E ricordiamoci che `console.log()` è nostro amico!
 Buon lavoro!
 DATI:
+
+
 Wayne Barnett	Founder & CEO	wayne-barnett-founder-ceo.jpg
+
 Angela Caroll	Chief Editor	angela-caroll-chief-editor.jpg
+
 Walter Gordon	Office Manager	walter-gordon-office-manager.jpg
-Angela Lopez	Social Media Manager	angela-lopez-social-media-manager.jpg
+
+Angela Lopez	Social Media Manager	angela-lopez-social-media-manager.
+jpg
+
 Scott Estrada	Developer	scott-estrada-developer.jpg
+
 Barbara Ramos	Graphic Designer	barbara-ramos-graphic-designer.jpg -->
---
+++ js/script.js
@@ -0,0 +1,55 @@
+// 1 Creo l'array di oggetti con le varie informazioni
+// 2 Stampo su console le varie informazioni e la stringa della foto
+// 3 Stampo le stesse cose del punto 2 su DOM 
+
+
+
+// 1 
+
+const arrTeam = [
+  {
+    nome : 'Wayne Barnett' , 
+    ruolo : 'Founder & CEO' ,
+    foto : 'wayne-barnett-founder-ceo.jpg'
+  },
+  {
+    nome : 'Angela Caroll' , 
+    ruolo : 'Chief Editor' ,
+    foto : 'angela-caroll-chief-editor.jpg'
+  },
+  {
+    nome : 'Walter Gordon' , 
+    ruolo : 'Office Manager' ,
+    foto : 'walter-gordon-office-manager.jpg'
+  },
+  {
+    nome : 'Angela Lopez' , 
+    ruolo : 'Social Media Manager' ,
+    foto : 'angela-lopez-social-media-manager.jpg'
+  },
+  {
+    nome : 'Scott Estrada' , 
+    ruolo : 'Developer' ,
+    foto : 'scott-estrada-developer.jpg'
+  },
+  {
+    nome : 'Barbara Ramos' , 
+    ruolo : 'Graphic Designer' ,
+    foto : 'barbara-ramos-graphic-designer.jpg'
+  }
+]
+
+
+const ul = document.querySelector('ul');
+
+
+for(let member of arrTeam){
+  // 2
+  console.log(arrTeam);
+
+  ul.innerHTML += `<li>${member.nome} , ${member.ruolo} , ${member.foto}</li>`
+}
+
+// 3
+
+
